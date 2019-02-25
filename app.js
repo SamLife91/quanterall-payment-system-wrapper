006428a90118518521502df018a3657be9c28128
SamLife91 <samir_barguti@hotmail.com>
2019-02-25 17:13:54 +0200

    - add node-fetch lib,

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/api/courses', (req, res) => {
-  res.send([1,2,3]);
+app.get('/api/courses', async (req, res) => {
+  const result = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  console.log(result);
+  res.send(result);
 });
 
 app.get('/api/courses/:id', (req, res) => {
