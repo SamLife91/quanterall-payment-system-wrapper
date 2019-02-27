1dd96b78a06e1d14382c6aac4ab43b4964c2c7d6
SamLife91 <samir_barguti@hotmail.com>
2019-02-27 12:10:35 +0200

    add register, add_device,  get_device in wrapper module

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-
-let qpsPath = `${process.cwd()}/qps/qps`
+/// module paths
+const qpsPath = `${process.cwd()}/qps/qps`
 const qps = require(qpsPath)
-
-
 
 app.use(express.json());
 
@@ -14,21 +12,72 @@ app.get('/', (req, res) => {
 
 app.post('/login', async (req, res) => {
   const body = {
-      type: "login",
-      operation: "auth",
-      data: {
-        user: "elvis",
-        password: "elvis"
-      }
-    } 
-  const alt = await qps.login(JSON.stringify(body))
-  console.log(alt)
-  res.send(alt);
+        type: "login",
+        operation: "auth",
+        data: {
+          user: "elvis",
+          password: "elvis"
+        }
+  }  
+  const result = await qps.login(JSON.stringify(body))
+  console.log(result)
+  res.send(result);
 });
 
-app.get('/api/courses/:id', (req, res) => {
+app.post('/register', async (req, res) => {
+  const body = {
+    type: 'register',
+    operation: 'auth',
+    data: {
+      user: 'arslan',
+      password: 'arslan_password',
+      mail: 'arslan@mail.arslan',
+      first_name: 'arslan',
+      last_name: 'arslan'
+    }
+  }
+  const result = await qps.register(JSON.stringify(body))
+  res.send(result);
+});
+
+app.post('/device/add', async (req, res) => {
+  const body = {
+    type: 'add_device',
+    operation: 'system',
+    auth: {
+      user: 'elvis',
+      token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyIjoiZWx2aXMiLCJleHAiOjE1NTE0MDI3NDZ9.AuKKWZ0z8qA8qSUIccw1lDHS-glR8_T7Rnhnpotys1vOFqkMXxeOlDDv4oVhHCnUT_PbGZ-sIxvNdEhMteN7BZUYaZ8tpW60VC_isUeokzp2E5pReByM2ytstAaKPcxkcHT8q1ZkQFaokWtaKs1QFgRtpsB4ykdNO3hqML9wEjAN9ZWGrgHNeADAv3t-CKWQ7FfXy6ryMRwOO5KbJ7DTJWrGeG42GYhYcMe86gU5XfeSqaf4NDdRyGujao4jWclY7KskvfEOhjR0HOiiR4iBVIemIqwamZyvuIij7qofxG7usCyy1w3MFCKsxdR7w0EwikZ0TDAuUWP2GhEeLMq6Mg'
+    },
+    data: {
+      type: 'fiscal',
+      manufacture: 'datecs',
+      model: 'fp-60kl',
+      serial_number: 'dt2742k873' 
+    }
+  }
+  const result = await qps.add_device(JSON.stringify(body))
+  res.send(result)
+})
+
+app.post('/device', async (req, res) => {
+  const body = {
+    type: 'get_all_devices',
+    operation: 'system',
+    auth: {
+      user: 'elvis',
+      token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyIjoiZWx2aXMiLCJleHAiOjE1NTE0MDI3NDZ9.AuKKWZ0z8qA8qSUIccw1lDHS-glR8_T7Rnhnpotys1vOFqkMXxeOlDDv4oVhHCnUT_PbGZ-sIxvNdEhMteN7BZUYaZ8tpW60VC_isUeokzp2E5pReByM2ytstAaKPcxkcHT8q1ZkQFaokWtaKs1QFgRtpsB4ykdNO3hqML9wEjAN9ZWGrgHNeADAv3t-CKWQ7FfXy6ryMRwOO5KbJ7DTJWrGeG42GYhYcMe86gU5XfeSqaf4NDdRyGujao4jWclY7KskvfEOhjR0HOiiR4iBVIemIqwamZyvuIij7qofxG7usCyy1w3MFCKsxdR7w0EwikZ0TDAuUWP2GhEeLMq6Mg'
+    }
+  }
+
+  const result = await qps.add_device(JSON.stringify(body))
+  res.send(result)
+})
+
+app.post('/api/courses', (req, res) => {
   // get route params
-  res.send(req.params);
+  // console.log(req)
+  console.log(req.body)
+  res.send(req.body);
 })
 
 // PORT
