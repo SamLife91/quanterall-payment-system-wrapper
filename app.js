5d1858d82509e8c03a0c5fe361f0aa4dad8b2a61
SamLife91 <samir_barguti@hotmail.com>
2019-02-28 16:47:34 +0200

    refactor app router

--- app.js
+++ app.js
@@ -10,74 +10,33 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.post('/login', async (req, res) => {
-  const body = {
-        type: "login",
-        operation: "auth",
-        data: {
-          user: "elvis",
-          password: "elvis"
-        }
-  }  
+app.post('/api/v1/login', async (req, res) => {
   const result = await qps.login(JSON.stringify(body))
   console.log(result)
   res.send(result);
 });
 
-app.post('/register', async (req, res) => {
-  const body = {
-    type: 'register',
-    operation: 'auth',
-    data: {
-      user: 'arslan',
-      password: 'arslan_password',
-      mail: 'arslan@mail.arslan',
-      first_name: 'arslan',
-      last_name: 'arslan'
-    }
-  }
+app.post('/api/v1/register', async (req, res) => {
   const result = await qps.register(JSON.stringify(body))
   res.send(result);
 });
 
-app.post('/device/add', async (req, res) => {
-  const body = {
-    type: 'add_device',
-    operation: 'system',
-    auth: {
-      user: 'elvis',
-      token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyIjoiZWx2aXMiLCJleHAiOjE1NTE0MDI3NDZ9.AuKKWZ0z8qA8qSUIccw1lDHS-glR8_T7Rnhnpotys1vOFqkMXxeOlDDv4oVhHCnUT_PbGZ-sIxvNdEhMteN7BZUYaZ8tpW60VC_isUeokzp2E5pReByM2ytstAaKPcxkcHT8q1ZkQFaokWtaKs1QFgRtpsB4ykdNO3hqML9wEjAN9ZWGrgHNeADAv3t-CKWQ7FfXy6ryMRwOO5KbJ7DTJWrGeG42GYhYcMe86gU5XfeSqaf4NDdRyGujao4jWclY7KskvfEOhjR0HOiiR4iBVIemIqwamZyvuIij7qofxG7usCyy1w3MFCKsxdR7w0EwikZ0TDAuUWP2GhEeLMq6Mg'
-    },
-    data: {
-      type: 'fiscal',
-      manufacture: 'datecs',
-      model: 'fp-60kl',
-      serial_number: 'dt2742k873' 
-    }
-  }
+app.post('/api/v1/device/add', async (req, res) => {
   const result = await qps.add_device(JSON.stringify(body))
   res.send(result)
 })
 
-app.post('/device', async (req, res) => {
-  const body = {
-    type: 'get_all_devices',
-    operation: 'system',
-    auth: {
-      user: 'elvis',
-      token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyIjoiZWx2aXMiLCJleHAiOjE1NTE0MDI3NDZ9.AuKKWZ0z8qA8qSUIccw1lDHS-glR8_T7Rnhnpotys1vOFqkMXxeOlDDv4oVhHCnUT_PbGZ-sIxvNdEhMteN7BZUYaZ8tpW60VC_isUeokzp2E5pReByM2ytstAaKPcxkcHT8q1ZkQFaokWtaKs1QFgRtpsB4ykdNO3hqML9wEjAN9ZWGrgHNeADAv3t-CKWQ7FfXy6ryMRwOO5KbJ7DTJWrGeG42GYhYcMe86gU5XfeSqaf4NDdRyGujao4jWclY7KskvfEOhjR0HOiiR4iBVIemIqwamZyvuIij7qofxG7usCyy1w3MFCKsxdR7w0EwikZ0TDAuUWP2GhEeLMq6Mg'
-    }
-  }
-
-  const result = await qps.add_device(JSON.stringify(body))
+app.post('/api/v1/device', async (req, res) => {
+  const result = await qps.get_device(JSON.stringify(body))
   res.send(result)
 })
 
-app.post('/api/courses', (req, res) => {
-  // get route params
-  // console.log(req)
-  console.log(req.body)
-  res.send(req.body);
+app.post('/api/v1/item/add', async (req, res) => {
+  const result = await qps.add_item(JSON.stringify(body), res)
+})
+
+app.post('/api/v1/item/get', async (req, res) => {
+  
 })
 
 // PORT
--- qps/qps.js
+++ qps/qps.js
@@ -48,6 +48,7 @@ async function add_device (data) {
 }
 
 async function get_device (data) {
+  console.log(data)
   const pckg = msgpack.pack(data)
   const packgBase64 = pckg.toString('base64')
 
@@ -62,8 +63,72 @@ async function get_device (data) {
   return msgpack.unpack(result)
 }
 
+async function add_item (data, res) {
+  const pckg = msgpack.pack(data)
+  const packgBase64 = pckg.toString('base64')
+
+  let options = {
+    method: 'POST',
+    url: baseURL,
+    body: packgBase64,
+    json: false
+  }
+
+  const result = await rp(options).then(res => Buffer(res, 'base64')).catch(err => console.log(err.msg))
+  console.log(msgpack.unpack(result))
+  let json = JSON.parse(data)
+  const item = msgpack.unpack(result)
+  if (item.status === 'failed') {
+    return res.status(400).send(item.response.message)
+  } else {
+    const decodeBody = {
+      type: 'add_item',
+      operation: 'parse_response',
+      auth: {
+        user: json.auth.user,
+        token: json.auth.token
+      },
+      data: {
+        device: {
+          id: json.data.device.id
+        },
+        data: item.response.message[0]
+      }
+    }
+    const alternativeData = JSON.stringify(decodeBody)
+    const pckg2 = msgpack.pack(alternativeData)
+    const packgBase642 = pckg2.toString('base64')
+  
+    let decodeOptions = {
+      method: 'POST',
+      url: baseURL,
+      body: packgBase642,
+      json: false
+    }
+    const decoded = await rp(decodeOptions).then(res => Buffer(res, 'base64')).catch(err => console.log(err.msg))
+    return res.status(200).send(msgpack.unpack(decoded))
+  }
+  
+  
+} 
+
+async function get_item (data) {
+  const pckg = msgpack.pack(data)
+  const packgBase64 = pckg.toString('base64')
+
+  let options = {
+    method: 'POST',
+    url: baseURL,
+    body: packgBase64,
+    json: false
+  }
+
+  const result = await rp(options).then(res => Buffer(res, 'base64')).catch(err => console.log(err.msg))
+  return msgpack.unpack(result)
+}
+
 
 // module.exports = {
 //   login
 // }
-module.exports = {login, register, add_device, get_device}
+module.exports = {login, register, add_device, get_device, get_item, add_item}
