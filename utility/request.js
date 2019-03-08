ed115b72086fb48f9097bee657238e232f618c1b
SamLife91 <samir_barguti@hotmail.com>
2019-03-08 17:08:33 +0200

    clean seriliazation

--- utility/request.js
+++ utility/request.js
@@ -1,5 +1,7 @@
-const port = '8808'
-const baseURL = `http://31.13.251.48:${port}`
+const port = '8808';
+const printerPort = '8800'
+const baseURL = `http://31.13.251.48:${port}`;
+const fiscalURL = `http://192.168.0.150:${printerPort}`;
 
 function generate_request_options (body) {
   return {
@@ -10,6 +12,16 @@ function generate_request_options (body) {
   }
 }
 
+function generate_fiscal_device_options (body) {
+  console.log(body)
+  return {
+    method: 'POST',
+    url: fiscalURL,
+    body: body,
+    json: false
+  }
+}
 module.exports = {
-  generate_request_options
+  generate_request_options,
+  generate_fiscal_device_options
 }
