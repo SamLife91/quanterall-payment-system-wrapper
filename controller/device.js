f7e2d92b7e7edac9c8e661355045a1b67970136b
SamLife91 <samir_barguti@hotmail.com>
2019-03-12 17:07:18 +0200

    set and get time routes and controller

--- controller/device.js
+++ controller/device.js
@@ -78,9 +78,74 @@ async function status (req, res) {
   res.status(200).send(device_status);
 }
 
+async function get_time (req, res) {
+  const options = request.generate_request_options(JSON.stringify(req.body));
+  const response = await rp(options).then(res => JSON.parse(res));
+  // res.send(response)
+  const fiscal_command = base64.decode(response.response.message[0]);
+  const fiscal_option = request.generate_fiscal_device_options(fiscal_command);
+  const fiscal_response = await rp(fiscal_option).then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err));
+  // res.send(fiscal_response)
+
+  const fiscal_body_response = {
+    operation: "parse_response",
+    type: 'get_time_and_date',
+    auth: {
+      user: req.body.auth.user,
+      token: req.body.auth.token
+    },
+    data: {
+      device: {
+        id: req.body.data.device.id
+      },
+      data: fiscal_response.message
+    }
+  }
+
+  const get_fiscal_time_options = request.generate_request_options(JSON.stringify(fiscal_body_response));
+  const get_fiscal_time_request = await rp(get_fiscal_time_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
+  res.send(get_fiscal_time_request);
+}
+
+async function set_time (req, res) {
+  const options = request.generate_request_options(JSON.stringify(req.body));
+  const response = await rp(options).then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err));
+  // res.send(response);
+  console.log(response)
+  const fiscal_command = base64.decode(response.response.message[0]);
+  const fiscal_option = request.generate_fiscal_device_options(fiscal_command);
+  const fiscal_response = await rp(fiscal_option).then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err));
+
+    console.log(fiscal_response)
+
+    const fiscal_body_response = {
+      operation: "parse_response",
+      type: 'get_time_and_date',
+      auth: {
+        user: req.body.auth.user,
+        token: req.body.auth.token
+      },
+      data: {
+        device: {
+          id: req.body.data.device.id
+        },
+        data: fiscal_response.message
+      }
+    }
+  
+    const set_fiscal_time_options = request.generate_request_options(JSON.stringify(fiscal_body_response));
+    const set_fiscal_time_request = await rp(set_fiscal_time_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
+    res.send(set_fiscal_time_request);
+}
+
 module.exports = {
   add_device,
   get_device,
   activate_device,
-  status
+  status,
+  get_time,
+  set_time
 }
--- routes/device.js
+++ routes/device.js
@@ -6,5 +6,7 @@ router.post('/list', device.get_device)
 router.post('/add', device.add_device)
 router.post('/activate', device.activate_device)
 router.post('/status', device.status)
+router.post('/get-time',device.get_time)
+router.post('/set-time', device.set_time)
 
 module.exports = router
