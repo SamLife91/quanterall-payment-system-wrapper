4ff625f7f02f97ba0e63605630a8f2668697739c
SamLife91 <samir_barguti@hotmail.com>
2019-03-13 16:05:52 +0200

    refactor controllers with new body generation fn

--- controller/device.js
+++ controller/device.js
@@ -1,49 +1,31 @@
-// const serialization = require('../utility/serialization');
+const rp = require('request-promise');
 const request = require('../utility/request');
 const base64 = require('../utility/base64');
-
-const rp = require('request-promise');
 
 async function add_device (req, res) {
   const options = request.generate_request_options(JSON.stringify(req.body));
   const response = await rp(options)
-        .then(res => JSON.parse(res));
-  if (response.status === 'failed') {
-    res.status(403).send(response.response)
-  } else {
-    const fiscal_command = base64.decode(response.response.message.activate_command)
-    const fiscal_options = request.generate_fiscal_device_options(fiscal_command)
-    const fiscal_response = await rp(fiscal_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
-    const activate_device_body = {
-      auth: {
-        token: req.body.auth.token,
-        user: req.body.auth.user
-      },
-      data: {
-        data: fiscal_response.message,
-        device: {
-          id: response.response.message.device_id
-        }
-      },
-      operation: 'parse_response',
-      type: 'add_device'
-    }
-    
-    const active_device_options = request.generate_request_options(JSON.stringify(activate_device_body))
-    const active_device_request = await rp(active_device_options).then(res => JSON.parse(res)).catch(err => console.log(err))
-    res.send(active_device_request)
-  }
+    .then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err));
+  const fiscal_command = base64.decode(response.response.message.activate_command)
+  const fiscal_options = request.generate_fiscal_device_options(fiscal_command)
+  const fiscal_response = await rp(fiscal_options)
+    .then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err))
+  
+  const params = request.generate_parse_response_params('add_device', req, fiscal_response.message)
+  const parse_response_body = request.generate_body_from_fiscal_response(params)
+  const parse_response_options = request.generate_request_options(JSON.stringify(parse_response_body))
+  const parse_response_request = await rp(parse_response_options)
+    .then(res => JSON.parse(res))
+    .catch(err => console.log(err))
+  res.send(parse_response_request)
 }
 
 async function get_device (req, res) {
   const options = request.generate_request_options(JSON.stringify(req.body))
-  const response = await rp(options)
-        .then(res => JSON.parse(res));
-  if (response.status === 'failed') {
-    res.status(403).send(response.response.message)
-  } else {
-    res.send(response)
-  }
+  const response = await rp(options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
+  res.send(response)
 }
 
 async function activate_device (req, res) {
@@ -59,86 +41,49 @@ async function status (req, res) {
   const fiscal_options = request.generate_fiscal_device_options(fiscal_command)
   const fiscal_response = await rp(fiscal_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
   
-  const device_command_body = {
-    operation: "parse_response",
-    type: "device_status",
-    auth: {
-      user: req.body.auth.user,
-      token: req.body.auth.token
-    },
-    data: {
-      device: {
-        id: req.body.data.device.id
-      },
-      data: fiscal_response.message
-    }
-  }
-  const device_status_options = request.generate_request_options(JSON.stringify(device_command_body))
-  const device_status = await rp(device_status_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err));
-  res.status(200).send(device_status);
+  const params = request.generate_parse_response_params('device_status', req, fiscal_response.message)
+
+  const parse_response_body = request.generate_body_from_fiscal_response(params);
+  console.log(parse_response_body)
+  const parse_response_options = request.generate_request_options(JSON.stringify(parse_response_body))
+  const parse_response_request = await rp(parse_response_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err));
+  res.status(200).send(parse_response_request);
 }
 
 async function get_time (req, res) {
   const options = request.generate_request_options(JSON.stringify(req.body));
   const response = await rp(options).then(res => JSON.parse(res));
-  // res.send(response)
+
   const fiscal_command = base64.decode(response.response.message[0]);
   const fiscal_option = request.generate_fiscal_device_options(fiscal_command);
   const fiscal_response = await rp(fiscal_option).then(res => JSON.parse(res))
     .catch(err => res.status(500).send(err));
-  // res.send(fiscal_response)
 
-  const fiscal_body_response = {
-    operation: "parse_response",
-    type: 'get_time_and_date',
-    auth: {
-      user: req.body.auth.user,
-      token: req.body.auth.token
-    },
-    data: {
-      device: {
-        id: req.body.data.device.id
-      },
-      data: fiscal_response.message
-    }
-  }
-
-  const get_fiscal_time_options = request.generate_request_options(JSON.stringify(fiscal_body_response));
-  const get_fiscal_time_request = await rp(get_fiscal_time_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
-  res.send(get_fiscal_time_request);
+  const params = request.generate_parse_response_params('get_time_and_date', req, fiscal_response.message);
+  const parse_response_body = request.generate_body_from_fiscal_response(params);
+  const parse_response_options = request.generate_request_options(JSON.stringify(parse_response_body));
+  const parse_response_request = await rp(parse_response_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
+  res.send(parse_response_request);
 }
 
 async function set_time (req, res) {
   const options = request.generate_request_options(JSON.stringify(req.body));
   const response = await rp(options).then(res => JSON.parse(res))
     .catch(err => res.status(500).send(err));
-  // res.send(response);
-  console.log(response)
+    
   const fiscal_command = base64.decode(response.response.message[0]);
   const fiscal_option = request.generate_fiscal_device_options(fiscal_command);
   const fiscal_response = await rp(fiscal_option).then(res => JSON.parse(res))
     .catch(err => res.status(500).send(err));
 
-    console.log(fiscal_response)
-
-    const fiscal_body_response = {
-      operation: "parse_response",
-      type: 'get_time_and_date',
-      auth: {
-        user: req.body.auth.user,
-        token: req.body.auth.token
-      },
-      data: {
-        device: {
-          id: req.body.data.device.id
-        },
-        data: fiscal_response.message
-      }
-    }
-  
-    const set_fiscal_time_options = request.generate_request_options(JSON.stringify(fiscal_body_response));
-    const set_fiscal_time_request = await rp(set_fiscal_time_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
-    res.send(set_fiscal_time_request);
+  const params = request.generate_parse_response_params('set_time_and_date', req, fiscal_response.message);
+  const parse_response_body = request.generate_body_from_fiscal_response(params);
+     
+  const parse_response_options = request.generate_request_options(JSON.stringify(parse_response_body));
+  const parse_response_request = await rp(parse_response_options)
+    .then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err))
+  res.send(parse_response_request);
 }
 
 module.exports = {
--- controller/item.js
+++ controller/item.js
@@ -1,111 +1,75 @@
-const serialization = require('../utility/serialization');
+const rp = require('request-promise');
 const request = require('../utility/request');
 const base64 = require('../utility/base64')
 
-const msgpack = require('msgpack');
-const lite = require('msgpack-lite')
-
-const rp = require('request-promise');
-
 async function add_item(req, res) {
   const options = request.generate_request_options(JSON.stringify(req.body))
-  const response = await rp(options).then(res => JSON.parse(res)).catch(err => res.status(403).send(err));
-  console.log(response)
-  if (response.status === 'failed') {
-    res.status(403).send(response)
-  } else {
-    const fiscal_command = base64.decode(response.response.message[0])
-    const fiscal_options = request.generate_fiscal_device_options(fiscal_command);
-    const fiscal_response = await rp(fiscal_options).then(res => JSON.parse(res)).catch((err) => res.status(500).send(err));
+  const response = await rp(options)
+    .then(res => JSON.parse(res))
+    .catch(err => res.status(403).send(err));
+
+  const fiscal_command = base64.decode(response.response.message[0])
+  const fiscal_options = request.generate_fiscal_device_options(fiscal_command);
+  const fiscal_response = await rp(fiscal_options)
+    .then(res => JSON.parse(res))
+    .catch((err) => res.status(500).send(err));
+
+  const params = request.generate_parse_response_params('add_item', req, fiscal_response.message);
+  const parse_response_body = request.generate_body_from_fiscal_response(params)
     
-    const fiscal_body = {
-      type: 'add_item',
-      operation: 'parse_response',
-      auth: {
-        user: req.body.auth.user,
-        token: req.body.auth.token
-      },
-      data: {
-        device: {
-          id: req.body.data.device.id
-        },
-        data: fiscal_response.message
-      }
-    }
-    const add_item_options = request.generate_request_options(JSON.stringify(fiscal_body))
-    const add_item_request = await rp(add_item_options).then(res => JSON.parse(res)).catch((err) => res.status(500).send(err));
-    res.status(200).send(add_item_request);
-  }
+  const parse_response_options = request.generate_request_options(JSON.stringify(parse_response_body))
+  const parse_response_request = await rp(parse_response_options)
+    .then(res => JSON.parse(res))
+    .catch((err) => res.status(500).send(err));
+  res.status(200).send(parse_response_request);
 }
 
 // get item
 async function get_item (req, res) {
   const options = request.generate_request_options(JSON.stringify(req.body))
-  const response = await rp(options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
+  const response = await rp(options)
+    .then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err))
   
   const fiscal_command = base64.decode(response.response.message[0])
   const fiscal_options = request.generate_fiscal_device_options(fiscal_command)
-  const fiscal_response = await rp(fiscal_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
-  console.log(fiscal_response)
+  const fiscal_response = await rp(fiscal_options)
+    .then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err))
 
-  const get_item_body = {
-    operation: 'parse_response',
-    type: 'get_item',
-    auth: {
-      user: req.body.auth.user,
-      token: req.body.auth.token,
-    },
-    data: {
-      device: {
-        id: req.body.data.device.id
-      },
-      data: fiscal_response.message
-    }
-  }
+  const params = request.generate_parse_response_params('get_item', req, fiscal_response.message);
+  const parse_response_body = request.generate_body_from_fiscal_response(params)
 
-  const get_item_options = request.generate_request_options(JSON.stringify(get_item_body))
-  const get_item_response = await rp(get_item_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
-  res.send(get_item_response)
+  const parse_response_options = request.generate_request_options(JSON.stringify(parse_response_body))
+  const parse_response_response = await rp(parse_response_options)
+    .then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err))
+  res.send(parse_response_response)
 }
 
-
-// get all items
 async function get_all_items (req, res) {
   let items = [];
   
   const first_item_options = request.generate_request_options(JSON.stringify(req.body))
   const first_item = await rp(first_item_options).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
-  // res.send(first_item)
   const first_item_fiscal_command = base64.decode(first_item.response.message[0])
   const first_item_fiscal_options = request.generate_fiscal_device_options(first_item_fiscal_command)
   const first_item_fiscal_request = await rp(first_item_fiscal_options)
-        .then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
+    .then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err))
 
+  const params = request.generate_parse_response_params('get_first_item', req, first_item_fiscal_request.message);
+  const parse_response_first_item_body = request.generate_body_from_fiscal_response(params);
 
-  const first_item_body = {
-    operation: 'parse_response',
-    type: 'get_first_item',
-    auth: {
-      user: req.body.auth.user,
-      token: req.body.auth.token
-    },
-    data: {
-      device: {
-        id: req.body.data.device.id
-      },
-      data: first_item_fiscal_request.message
-    }
-  }
+  const parse_response_options = request.generate_request_options(JSON.stringify(parse_response_first_item_body))
+  const parse_response_request = await rp(parse_response_options)
+    .then(res => JSON.parse(res))
+    .catch(err => res.status(500).send(err))
 
-  const first_item_options_2 = request.generate_request_options(JSON.stringify(first_item_body))
-  const first_item_request_2 = await rp(first_item_options_2).then(res => JSON.parse(res)).catch(err => res.status(500).send(err))
-
-  // res.send(first_item_request_2)
-  items.push(first_item_request_2.response.message)
+  items.push(parse_response_request.response.message)
 
   let getNext = true
   do {
-    console.log('i')
     const next_item_body = {
       operation: 'build_command',
       type: 'get_next_item',
@@ -120,44 +84,29 @@ async function get_all_items (req, res) {
       }
     }
 
-    console.log(next_item_body)
-
     const next_item_option = request.generate_request_options(JSON.stringify(next_item_body))
-    const next_item_response = await rp(next_item_option).then(res => JSON.parse(res)).catch(err => err)
-    console.log(next_item_response.response.message[0]);
+    const next_item_response = await rp(next_item_option)
+      .then(res => JSON.parse(res))
+      .catch(err => res.status(500).send(err));
     
     const next_item_fiscal_command = base64.decode(next_item_response.response.message[0])
     const next_item_fiscal_options = request.generate_fiscal_device_options(next_item_fiscal_command)
-    const next_item_fiscal_response = await rp(next_item_fiscal_options).then(res => JSON.parse(res)).catch(err => console.log(err))
+    const next_item_fiscal_response = await rp(next_item_fiscal_options)
+      .then(res => JSON.parse(res))
+      .catch(err => console.log(err))
 
-
-    const next_item_body2 = {
-      operation: 'parse_response',
-      type: 'get_next_item',
-      auth: {
-        user: req.body.auth.user,
-        token: req.body.auth.token
-      },
-      data: {
-        device: {
-          id: req.body.data.device.id
-        },
-        data: next_item_fiscal_response.message
-      }
-    }
-
-    const next_item_options = request.generate_request_options(JSON.stringify(next_item_body2))
-
-    const next_item_response2 = await rp(next_item_options).then(res => JSON.parse(res))
-    console.log('**********')
-      console.log(next_item_response2.response.message)
-      console.log('**********')
-    if (next_item_response2.response.message === 'item_not_found') {
+    const params_next_item = request.generate_parse_response_params('get_next_item', req, next_item_fiscal_response.message)
+    const parse_response_next_item_body = request.generate_body_from_fiscal_response(params_next_item)
+    const parse_respone_next_item_options = request.generate_request_options(JSON.stringify(parse_response_next_item_body))
+    const parse_response_next_item_request = await rp(parse_respone_next_item_options)
+      .then(res => JSON.parse(res))
+      .catch(err => res.status(500).send(err));
+      console.log(parse_response_next_item_request.response);
+    if (parse_response_next_item_request.response.message === 'item_not_found') {
       getNext = false
     } else {
-      items.push(next_item_response2.response.message)
+      items.push(parse_response_next_item_request.response.message)
     }
-    console.log(getNext)
   } while (getNext);
 
   res.status(200).send(items)
