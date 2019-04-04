238653e5b4f8038a30f820d229a01474c7b79c2b
SamLife91 <samir_barguti@hotmail.com>
2019-04-04 09:57:04 +0300

    add axios (receipt)

--- controller/receipt.js
+++ controller/receipt.js
@@ -1,93 +1,111 @@
 const request = require('../utility/request');
 const base64 = require('../utility/base64');
-const rp = require('request-promise');
 const { forEach } = require('p-iteration');
+const axios = require('axios')
 
-async function fiscal (req, res) {
-  const options = request.gen_options(JSON.stringify(req.body));
-  const response = await rp(options).then(res => JSON.parse(res))
-  .catch(err => res.status(500).send(err))
-  const fiscal_commands = response.response.message.map(item => base64.decode(item));
-  let com = [];
+const baseURL = request.baseURL
+const fiscalURL = request.fiscalURL;
 
-  await forEach(fiscal_commands, async (command) => {
-    const fiscal_option = request.gen_device_options(command);
-    const fiscal_response = await rp(fiscal_option).then(res => JSON.parse(res)).catch(err => err);
-    com.push(fiscal_response)
+async function fiscal (req, res, next) { 
+  const result = await axios.post(baseURL, req.body)
+  .catch(() => {
+    const error = new Error('service is down')
+    return next(error)
+  })
+  if (!Array.isArray(result.data.response.message)) {
+    const error = new Error('device error')
+    return next(error)
+  }
+  const commands = result.data.response.message.map(item => base64.decode(item))
+  let fs_commands = [];
+
+  await forEach(commands, async (command) => {
+    const fs_result = await axios.post(fiscalURL, command)
+    .catch(() => {
+      const error = new Error('device error')
+      return next(error)
+    })
+    fs_commands.push(fs_result.data)
   })
 
   let parsed_commands = [];
-  await forEach(com, async (el) => {
+  await forEach(fs_commands, async (el) => {
     const params = request.gen_params('fiscal_receipt', req, el.message);
-    const parse_response_body = request.gen_body(params)
-    const fiscal_response_option = request.gen_options(JSON.stringify(parse_response_body));
-    const fiscal = await rp(fiscal_response_option).then(res => JSON.parse(res))
-      .catch(err => res.status(500).send(err))
-    parsed_commands.push(fiscal);
+    const pr_body = request.gen_body(params)
+    const fiscal = await axios.post(baseURL, pr_body).catch(() => {})
+    parsed_commands.push(fiscal.data);
+    console.log(parsed_commands)
   })
   res.send(parsed_commands);
 }
 
-async function non_fiscal(req, res) {
-  const options = request.gen_options(JSON.stringify(req.body));
-  const response = await rp(options)
-    .then(res => JSON.parse(res))
-    .catch(err => res.status(500));
-  if (!Array.isArray(response.response.message)) {
+async function non_fiscal(req, res, next) {
+  const result = await axios.post(baseURL, req.body)
+  .catch(() => {
+    const error = new Error('service is down')
+    return next(error)
+  })
+  if (!Array.isArray(result.data.response.message)) {
     res.status(403).send(res)
   } else {
-    const decoded_commands = response.response.message.map(item => base64.decode(item));
-    let fiscal_commands = [];
-    await forEach(decoded_commands, async (command) => {
-      const fiscal_option = request.gen_device_options(command);
-      const fiscal_response = await rp(fiscal_option)
-        .then(res => JSON.parse(res))
-        .catch(err => console.log(err));
-      fiscal_commands.push(fiscal_response.message);
+    const commands = result.data.response.message.map(item => base64.decode(item));
+    let fs_commands = [];
+    await forEach(commands, async (command) => {
+      const fs_result = await axios.post(fiscalURL, command)
+      .catch(() => {
+        const error = new Error('device error')
+        return next(error)
+      })
+      fs_commands.push(fs_result.data);
     })
+    console.log(fs_commands)
     let parsed_commnads = [];
-    await forEach(fiscal_commands, async (command) => {
-      const params = request.gen_params('non_fiscal_receipt', req, command);
-      const parse_response_body = request.gen_body(params);
-      const parse_response_options = request.gen_options(JSON.stringify(parse_response_body));
-      const parse_response = await rp(parse_response_options)
-        .then(res => JSON.parse(res))
-        .catch(err => console.log(err));
-      parsed_commnads.push(parse_response)
+    await forEach(fs_commands, async (command) => {
+      const params = request.gen_params('non_fiscal_receipt', req, command.message);
+      const pr_body = request.gen_body(params);
+      const pr_result = await axios.post(baseURL, pr_body)
+      .catch(() => {
+        const error = new Error('service is down')
+        return next(error)
+      })
+      parsed_commnads.push(pr_result.data)
     })
     res.send(parsed_commnads);
   }
 }
 
-async function reversal (req, res) {
-  const options = request.gen_options(JSON.stringify(req.body));
-  const response = await rp(options)
-    .then(res => JSON.parse(res))
-    .catch(err => res.status(500).send(err));
+async function reversal (req, res, next) {
+  const result = await axios.post(baseURL, req.body)
+  .catch(() => {
+    const error = new Error('service is down')
+    return next(error)
+  })
 
-  if (!Array.isArray(response.response.message)) {
-    res.status(403).send(response.response.message)
+  if (!Array.isArray(result.data.response.message)) {
+    const error = new Error(result.data.response.message)
+    return next(error)
   } else {
-    const decoded_commands = response.response.message.map(item => base64.decode(item));
-  let fiscal_commands = [];
-  
-  await forEach(decoded_commands, async (command) => {
-    const fiscal_option = request.gen_device_options(command);
-    const fiscal_response = await rp(fiscal_option)
-      .then(res => JSON.parse(res))
-      .catch(err => console.log(err));
-    fiscal_commands.push(fiscal_response.message);
+    const commands = result.data.response.message.map(item => base64.decode(item));
+    let fs_commands = [];
+  await forEach(commands, async (command) => {
+    const fs_command = await axios.post(fiscalURL, command)
+    .catch(() => {
+      const error = new Error('device error')
+      return next(error)
+    })
+    fs_commands.push(fs_command.data.message);
   })
 
   let parsed_commnads = [];
-  await forEach(fiscal_commands, async (command) => {
+  await forEach(fs_commands, async (command) => {
     const params = request.gen_params('reversal', req, command);
-    const parse_response_body = request.gen_body(params);
-    const parse_response_options = request.gen_options(JSON.stringify(parse_response_body));
-    const parse_response = await rp(parse_response_options)
-      .then(res => JSON.parse(res))
-      .catch(err => console.log(err));
-    parsed_commnads.push(parse_response)
+    const pr_body = request.gen_body(params);
+    const pr_result = await axios.post(baseURL, pr_body)
+    .catch(() => {
+      const error = new Error('service is down')
+      return next(error)
+    })
+    parsed_commnads.push(pr_result.data)
   })
   res.send(parsed_commnads);
   }
